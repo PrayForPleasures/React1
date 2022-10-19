5af4ccf630ef870952b593af597c1069339fc4dd
Dmitriy Yarchuck <dmitriyyarchuck@gmail.com>
2022-10-19 22:44:18 +0300

    hw

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,7 +4,7 @@ import styles from "../Styles/Message.css";
 export const Message = (props) => {
   return (
     <div className="message">
-      Hi, my name is {props.name} and I'm {props.age}!
+      Hi, my name is {props.name} and I'm {props.age}!!!
     </div>
   );
 };
